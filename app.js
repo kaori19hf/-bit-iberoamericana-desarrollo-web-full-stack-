64bdbfacd2a5e59651899154b06515c0eed65c23
kaori19hf <kaori19hf@hotmail.com>
2022-03-11 09:44:47 -0500

    cambio app.js

--- app.js
+++ app.js
@@ -1,9 +1,1 @@
 
-var micarro = new object()
-micarro.color="negro";
-micarro.velociad= 80;
-
-console.log()
-//function myFunction (){
-  // document.getElementById("facebook").innerHTML= "The Coffee Shop";
-//}
