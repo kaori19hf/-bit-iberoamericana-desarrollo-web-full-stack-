57fa5570bbe68ff825f8684f52f5e505a203e6da
kaori19hf <kaori19hf@hotmail.com>
2022-03-10 22:08:15 -0500

    cambios css

---
+++ app.js
@@ -0,0 +1,9 @@
+
+var micarro = new object()
+micarro.color="negro";
+micarro.velociad= 80;
+
+console.log()
+//function myFunction (){
+  // document.getElementById("facebook").innerHTML= "The Coffee Shop";
+//}
